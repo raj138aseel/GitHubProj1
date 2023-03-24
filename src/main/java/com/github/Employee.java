686098a622b53e2e4809eaa2a88160a3ce0be4ce
rajas <rajas@Rajashekhar.Dlink>
2023-03-24 20:39:21 +0530

    Complete the implementation of EmployeeDAO interface

---
+++ src/main/java/com/github/Employee.java
@@ -0,0 +1,30 @@
+package com.github;
+public class Employee {
+	private int empId;
+	private String empName;
+	private int empSalary;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getEmpSalary() {
+		return empSalary;
+	}
+	public void setEmpSalary(int empSalary) {
+		this.empSalary = empSalary;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSalary=" + empSalary + "]";
+	}
+	
+}
+
